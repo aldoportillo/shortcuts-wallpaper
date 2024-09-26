7283582f9b8443b4c593b1045e396f98dc712e36
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2024-09-26 13:41:58 -0500

    Add head and doctype to generated html

--- convert.js
+++ convert.js
@@ -5,9 +5,25 @@ const markdownFile = './README.md';
 const markdownContent = fs.readFileSync(markdownFile, 'utf-8');
 
 console.log('Converting markdown to HTML...');
+const bodyContent = marked.parse(markdownContent);
 
-const htmlContent = marked.parse(markdownContent);
+const htmlTemplate = `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>Markdown to HTML</title>
+  <link ref="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/github-markdown-css/4.0.0/github-markdown.min.css">
+  <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+  ${bodyContent}
+</body>
+</html>
+`;
 
-fs.writeFileSync('./index.html', htmlContent);
+fs.writeFileSync('./index.html', htmlTemplate);
 
 console.log('Markdown converted to HTML successfully.');
