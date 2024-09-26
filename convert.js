7290df1a99a1a0fbb4911b765473c6c9a2803ac8
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2024-09-26 14:35:40 -0500

    Modify convert script

--- convert.js
+++ convert.js
@@ -1,11 +1,21 @@
 const fs = require('fs');
 const marked = require('marked');
 
-const markdownFile = './README.md';
+const markdownFile = './shortcuts.md';
 const markdownContent = fs.readFileSync(markdownFile, 'utf-8');
 
 console.log('Converting markdown to HTML...');
-const bodyContent = marked.parse(markdownContent);
+
+let bodyContent = marked.parse(markdownContent);
+
+bodyContent = bodyContent.replace(/<h2>(.*?)<\/h2>/g, '<div class="section"><h2>$1</h2>'); // Wrap h2 in div
+bodyContent = bodyContent.replace(/<h3>(.*?)<\/h3>/g, '<div class="list-group"><h3>$1</h3>'); // Wrap h3 in div
+
+bodyContent = bodyContent.replace(/<\/ul>/g, '</ul></div></div>'); // Close both list-group and section divs
+
+if (bodyContent.startsWith('</div>')) {
+  bodyContent = bodyContent.replace('</div>', '');
+}
 
 const htmlTemplate = `
 <!DOCTYPE html>
@@ -14,8 +24,7 @@ const htmlTemplate = `
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>Markdown to HTML</title>
-  <link ref="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/github-markdown-css/4.0.0/github-markdown.min.css">
+  <title>Shortcuts</title>
   <link rel="stylesheet" href="styles.css">
 </head>
 <body>
